e12bdfdcfadbb57dd55a33faf009835f4257537e
TheGoldKay <jhonnatanwfc@live.com>
2024-03-23 12:14:00 -0300

    add hex colors support

--- helper.lua
+++ helper.lua
@@ -12,4 +12,16 @@ function helper:rgb(r, g, b)
     return {r / 255, g / 255, b / 255}
 end
 
+function helper:hex(hex_code)
+    -- Remove the "#" symbol if present
+    hex_code = hex_code:gsub("#", "")
+    
+    -- Convert the hex code to RGB
+    local r = tonumber(hex_code:sub(1, 2), 16)
+    local g = tonumber(hex_code:sub(3, 4), 16)
+    local b = tonumber(hex_code:sub(5, 6), 16)
+    
+    return self:rgb(r, g, b)
+end
+
 return helper
